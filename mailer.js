3864e5144b50b56725e62afc4c3d4124ad007e1e
as3545 <86479682+as3545@users.noreply.github.com>
2025-09-25 19:49:26 +0530

    mailer.js

---
+++ mailer.js
@@ -0,0 +1,34 @@
+// utils/mailer.js
+const nodemailer = require("nodemailer");
+const emailUser = process.env.EMAIL_USER;
+const emailPassword = process.env.EMAIL_PASSWORD;
+const ownerEmail = process.env.OWNER_EMAIL;
+const transporter = nodemailer.createTransport({
+  service: "gmail", // or use SMTP config for production
+  auth: {
+    user: emailUser,
+    pass: emailPassword,
+  },
+});
+
+/**
+ * Send email
+ * @param {string} to - Recipient email
+ * @param {string} subject - Email subject
+ * @param {string} html - Email body (HTML)
+ */
+const sendEmail = async (to, subject, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Travel Agency" <${process.env.EMAIL_USER}>`,
+      to: ownerEmail,
+      subject,
+      html,
+    });
+    console.log("✅ Email sent:", info.messageId);
+  } catch (error) {
+    console.error("❌ Error sending email:", error);
+  }
+};
+
+module.exports = { sendEmail };
